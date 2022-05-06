8aae5ecb8db4620ab5c2a57aae24d88ac8ae2078
gabriel53 <copperfieldcover@gmail.com>
2022-05-07 00:45:06 +0800

    created header component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,9 +18,20 @@
     </head>
     <body class="font-sans antialiased">
         <div class="flex h-screen overflow-hidden">
+            {{-- Sidebar/Navigation --}}
             <aside id="sidebar" class="flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64  lg:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out -translate-x-64">
                 @include('components.sidebar')
             </aside>
+            <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                {{-- Header --}}
+                @include('components.header')
+                {{-- Contents --}}
+                <main>
+                    <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+                        {{ $slot }}
+                    </div>
+                </main>
+            </div>
         </div>
     </body>
 </html>
