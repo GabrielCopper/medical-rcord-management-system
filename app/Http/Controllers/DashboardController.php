aac2458d18e0d81535ccab2dd739d1bcd36380c4
gabriel53 <copperfieldcover@gmail.com>
2022-05-07 01:38:50 +0800

    added welcome banner and moved admin dashboard to its specific directory

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
     public function index()
     {
         if (Auth::user()->hasRole('administrator')) {
-            return view('pages.admin.dashboard');
+            return view('pages.admin.dashboard.index');
         } elseif (Auth::user()->hasRole('superadministrator')) {
             return view('pages.superadmin.dashboard');
         }
---
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    @include('pages.admin.dashboard.welcome-banner')
+</x-app-layout>
