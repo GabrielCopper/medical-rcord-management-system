b0ceca89aa830f623d5c2bccebba6ed0ae035fae
gabriel53 <copperfieldcover@gmail.com>
2022-05-07 09:21:55 +0800

    changed body background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" style="background-color: #F0F4F8">
         <div class="flex h-screen overflow-hidden">
             {{-- Sidebar/Navigation --}}
             <aside id="sidebar" class="flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64  lg:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out -translate-x-64">
