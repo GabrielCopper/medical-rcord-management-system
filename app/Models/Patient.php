8867f177e3885f7806976b12f1e74838ed8f768d
gabriel53 <copperfieldcover@gmail.com>
2022-05-20 22:00:09 +0800

    added patient columns in database using migration

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends Model
 {
     use HasFactory;
+
+     protected $fillable = [
+        'patient_id',
+        'patient_full_name',
+        'patient_gender',
+        'patient_role',
+        'patient_year',
+        'patient_department',
+        'patient_phone_number',
+        'patient_consult_date',
+        'patient_consult_time',
+        'patient_medical_comments',
+     ];
 }
--- database/migrations/2022_05_20_124039_create_patients_table.php
+++ database/migrations/2022_05_20_124039_create_patients_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
+            $table->string('patient_id');
+            $table->string('patient_full_name');
+            $table->string('patient_gender');
+            $table->string('patient_role');
+            $table->string('patient_year')->nullable();
+            $table->string('patient_department')->nullable();
+            $table->bigInteger('patient_phone_number');
+            $table->date('patient_consult_date');
+            $table->string('patient_consult_time');
+            $table->string('patient_medical_comments');
             $table->timestamps();
         });
     }
