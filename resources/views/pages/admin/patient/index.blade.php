45afe0f8d131724bf1f2e8e7ec4f4256684667c6
gabriel53 <copperfieldcover@gmail.com>
2022-05-20 22:51:55 +0800

    make create patient button to a link and moved to index patient page

--- resources/views/pages/admin/patient/index.blade.php
+++ resources/views/pages/admin/patient/index.blade.php
@@ -2,7 +2,14 @@
     <div class="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
         <header class="px-5 py-4 border-b border-slate-100 flex justify-between items-center">
             <h2 class="font-semibold text-slate-800">Patients</h2>
-            @include('pages.admin.patient.create-patient')
+            <a href=""
+                class="px-4 py-2 font-medium text-sm inline-flex items-center justify-center border border-transparent rounded leading-5 shadow-sm transition duration-150 ease-in-out bg-indigo-500 hover:bg-indigo-600 text-white">
+                <svg class="w-3 h-3 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                    <path
+                        d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                </svg>
+                <span class="xs:block text-sm ml-2">New Patient</span>
+            </a>
         </header>
         <div class="p-3">
             @include('pages.admin.patient.patient-table')
