3098789fc17a86fa3969721e231c67c31a504b40
gabriel53 <copperfieldcover@gmail.com>
2022-07-10 10:11:49 +0800

    Empty message for patient table

--- resources/views/pages/admin/patient/patient-table.blade.php
+++ resources/views/pages/admin/patient/patient-table.blade.php
@@ -1,4 +1,5 @@
 {{-- Table --}}
+@unless ($patients->isEmpty())
 <div class="overflow-x-auto">
     <table class="table-auto w-full">
         {{-- table header --}}
@@ -27,7 +28,7 @@
         {{-- table body --}}
         <tbody class="text-sm font-medium divide-y divide-slate-100">
             {{-- row --}}
-            @unless ($patients->isEmpty())
+
             @foreach ($patients as $patient)
             <tr>
                 <td class="p-2">
@@ -58,13 +59,19 @@
                 </td>
             </tr>
             @endforeach
-            @else
-            <tr class="border-gray-300">
-                <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                    <p class="text-center">No Patients Found</p>
-                </td>
-            </tr>
-            @endunless
         </tbody>
     </table>
 </div>
+@else
+<div class="flex-col flex items-center justify-center">
+    <div>
+        <img src="{{ asset('images/empty-illustrations/empty-patient-table.svg') }}"
+            alt="There are currently no listed patients.">
+    </div>
+    <div>
+        <h1 class="text-center font-bold text-xl mt-8 uppercase">
+            There are currently no listed patients.
+        </h1>
+    </div>
+</div>
+@endunless
