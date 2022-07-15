5c53c7452529973a3d83e2f142401d06dd5d5699
gabriel53 <copperfieldcover@gmail.com>
2022-07-15 16:36:49 +0800

    validation in editing user

--- resources/views/pages/admin/users/edit.blade.php
+++ resources/views/pages/admin/users/edit.blade.php
@@ -50,14 +50,41 @@
                 <div class="col-span-6 sm:col-span-2">
                     <label for="user_patient_id" class="text-sm text-gray-800 mb-1 block">User ID</label>
                     <input type="text" name="user_patient_id" id="user_patient_id" :value="old('user_patient_id')"
-                        value="{{$user->user_patient_id}}"
-                        class="text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4 py-2">
+                        value="{{$user->user_patient_id}}" class="{{($errors->first('user_patient_id') ? "
+                        border-red-600" : "border-gray-300" )}} text-sm dark-text font-medium capitalize w-full border
+                        border-gray-300 shadow rounded-md px-4 py-2">
+                    @error('user_patient_id')
+                    <div class="flex items-center gap-1 mt-1 ml-1">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="#cc0000">
+                                <path fill-rule="evenodd"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <p class="text-red-700 font-medium text-xs">{{ $message }}</p>
+                    </div>
+                    @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-2">
                     <label for="user_patient_full_name" class="text-sm text-gray-800 mb-1 block">Full name</label>
                     <input type="text" name="user_patient_full_name" id="user_patient_full_name"
                         :value="old('user_patient_full_name')" value="{{$user->user_patient_full_name}}"
-                        class="text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4 py-2">
+                        class="{{($errors->first('user_patient_full_name') ? " border-red-600" : "border-gray-300" )}}
+                        text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4
+                        py-2">
+                    @error('user_patient_full_name')
+                    <div class="flex items-center gap-1 mt-1 ml-1">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="#cc0000">
+                                <path fill-rule="evenodd"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <p class="text-red-700 font-medium text-xs">{{ $message }}</p>
+                    </div>
+                    @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-2">
                     <label for="user_patient_gender" class="text-sm text-gray-800 mb-1 block">Sex/Gender</label>
@@ -69,24 +96,73 @@
                             Female</option>
                     </select>
                 </div>
-                <div id="department_year_role" class="col-span-6 sm:col-span-2 hidden">
+                <div id="department_year_role" class="col-span-6 sm:col-span-2">
                     <label for="user_year_department_role" class="text-sm text-gray-800 mb-1 block" id="label">
+                        @if ($user->user_patient_role === 'student')
+                        Year & Section
+                        @elseif($user->user_patient_role === 'teaching_staff')
+                        Department
+                        @elseif($user->user_patient_role === 'non_teaching_staff')
+                        Role
+                        @endif
                     </label>
                     <input type="text" name="user_year_department_role" id="user_year_department_role"
                         autocomplete="patient-department" value="{{ $user->user_year_department_role }}"
-                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        class="{{($errors->first('user_year_department_role') ? " border-red-600" : "border-gray-300"
+                        )}} mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm
+                        border-gray-300 rounded-md">
+                    @error('user_year_department_role')
+                    <div class="flex items-center gap-1 mt-1 ml-1">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="#cc0000">
+                                <path fill-rule="evenodd"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <p class="text-red-700 font-medium text-xs">{{ $message }}</p>
+                    </div>
+                    @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-2">
                     <label for="patient_phone_number" class="text-sm text-gray-800 mb-1 block">Contact Number</label>
                     <input type="text" name="patient_phone_number" id="patient_phone_number"
                         :value="old('patient_phone_number')" value="{{ $user->patient_phone_number }}"
-                        class="text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4 py-2">
+                        class="{{($errors->first('patient_phone_number') ? " border-red-600" : "border-gray-300" )}}
+                        text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4
+                        py-2">
+                    @error('patient_phone_number')
+                    <div class="flex items-center gap-1 mt-1 ml-1">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="#cc0000">
+                                <path fill-rule="evenodd"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <p class="text-red-700 font-medium text-xs">{{ $message }}</p>
+                    </div>
+                    @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-2">
                     <label for="user_patient_birthday" class="text-sm text-gray-800 mb-1 block">Birth Date</label>
                     <input type="date" name="user_patient_birthday" id="user_patient_birthday"
                         :value="old('user_patient_birthday')" value="{{ $user->user_patient_birthday }}"
-                        class="text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4 py-2">
+                        class="{{($errors->first('user_patient_birthday') ? " border-red-600" : "border-gray-300" )}}
+                        text-sm dark-text font-medium capitalize w-full border border-gray-300 shadow rounded-md px-4
+                        py-2">
+                    @error('user_patient_birthday')
+                    <div class="flex items-center gap-1 mt-1 ml-1">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="#cc0000">
+                                <path fill-rule="evenodd"
+                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <p class="text-red-700 font-medium text-xs">{{ $message }}</p>
+                    </div>
+                    @enderror
                 </div>
                 <div class="col-span-6 sm:col-span-2">
                     <label for="user_patient_blood_type" class="text-sm text-gray-800 mb-1 block">Blood Type</label>
@@ -114,40 +190,20 @@
 <script>
     // This script tells what to do if you select a patient role
     // will hide some inputs or enable inputs
-    const patient_role = document.getElementById("patient_role").value;
+    const student = document.getElementById("student").value;
+    const teaching_staff = document.getElementById("teaching_staff").value;
+    const non_teaching_staff = document.getElementById("non_teaching_staff").value;
+    const label = document.getElementById("label");
 
     function update(that) {
-        const inputs = document.querySelectorAll(".inputs");
         if (that) {
-            const student = document.getElementById("student").value;
-            const teaching_staff =
-                document.getElementById("teaching_staff").value;
-            const non_teaching_staff =
-                document.getElementById("non_teaching_staff").value;
-            const label = document.getElementById("label");
-            const department_year_role = document.getElementById(
-                "department_year_role"
-            );
-
             if (teaching_staff === that.value) {
                 label.textContent = "Department";
-                department_year_role.style.display = "block";
             } else if (student === that.value) {
                 label.textContent = "Year & Section";
-                department_year_role.style.display = "block";
             } else if (non_teaching_staff === that.value) {
                 label.textContent = "Role";
-                department_year_role.style.display = "block";
-            } else {
-                department_year_role.style.display = "none";
             }
         }
-
-        inputs.forEach((input) => {
-            if (that.value !== "Patient Role") {
-                input.disabled = false;
-                input.style.cursor = "default";
-            }
-        });
     }
 </script>
