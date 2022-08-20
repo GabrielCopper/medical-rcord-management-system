a7066dd3a3032647d6ef8feed4cce14d0fe0e2b6
gabriel53 <copperfieldcover@gmail.com>
2022-08-21 00:48:37 +0800

    analytics for how many patients every month

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Analytics;
 use App\Http\Requests\StoreAnalyticsRequest;
 use App\Http\Requests\UpdateAnalyticsRequest;
+use App\Models\Patient;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class AnalyticsController extends Controller
 {
@@ -15,7 +18,23 @@ class AnalyticsController extends Controller
      */
     public function index()
     {
-        return view('pages.admin.analytics.index');
+    $january = Patient::whereMonth('patient_consult_date', '1')->count();
+    $february = Patient::whereMonth('patient_consult_date', '2')->count();
+    $march = Patient::whereMonth('patient_consult_date', '3')->count();
+    $april = Patient::whereMonth('patient_consult_date', '4')->count();
+    $may = Patient::whereMonth('patient_consult_date', '5')->count();
+    $june = Patient::whereMonth('patient_consult_date', '6')->count();
+    $july = Patient::whereMonth('patient_consult_date', '7')->count();
+    $august = Patient::whereMonth('patient_consult_date', '8')->count();
+    $september = Patient::whereMonth('patient_consult_date', '9')->count();
+    $october = Patient::whereMonth('patient_consult_date', '10')->count();
+    $november = Patient::whereMonth('patient_consult_date', '11')->count();
+    $december = Patient::whereMonth('patient_consult_date', '12')->count();
+
+
+    return view('pages.admin.analytics.index', compact(
+        'january','february','march','april','may','june','july','august','september','october','november','december',
+        ));
     }
 
     /**
--- resources/views/pages/admin/analytics/index.blade.php
+++ resources/views/pages/admin/analytics/index.blade.php
@@ -1,3 +1,49 @@
 <x-app-layout>
     @section('title','Analytics')
     <h1>Analytics</h1>
+    {{-- graphs --}}
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mt-4">
+            <canvas id="chartAge" width="400" height="100"></canvas>
+        </div>
+    </div>
+</x-app-layout>
+
+<script>
+    const ctx = document.getElementById('chartAge');
+            const chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [{
+            label: 'Number of Patients',
+            data: [{{ $january }}, {{ $february }}, {{ $march }}, {{ $april }}, {{ $may }}, {{ $june }}, {{ $july }}, {{ $august }}, {{ $september }}, {{ $october }}, {{ $november }}, {{ $december }}],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 1
+            }]
+            },
+            options: {
+            scales: {
+            y: {
+            beginAtZero: true
+            }
+
+            }
+            }
+
+            });
+</script>
