659cf5f5b1d07a7d32c47a18c8d61d74b8ee4395
gabriel53 <copperfieldcover@gmail.com>
2022-08-27 03:15:21 +0800

    format js code

--- resources/views/pages/admin/analytics/index.blade.php
+++ resources/views/pages/admin/analytics/index.blade.php
@@ -15,40 +15,42 @@
 
 <script>
     const ctx = document.getElementById('chartAge');
-            const chart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    const chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'
+            ],
             datasets: [{
-            label: 'Number of Patients',
-            data: [{{ $january }}, {{ $february }}, {{ $march }}, {{ $april }}, {{ $may }}, {{ $june }}, {{ $july }}, {{ $august }}, {{ $september }}, {{ $october }}, {{ $november }}, {{ $december }}],
-            backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            ],
-            borderWidth: 1
+                label: 'Number of Patients',
+                data: [
+                    {{ $january }}, {{ $february }}, {{ $march }}, {{ $april }},
+                    {{ $may }}, {{ $june }}, {{ $july }}, {{ $august }}, {{ $september }},
+                    {{ $october }}, {{ $november }}, {{ $december }}
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                ],
+                borderWidth: 1
             }]
-            },
-            options: {
+        },
+        options: {
             scales: {
-            y: {
-            beginAtZero: true
-            }
-
-            }
-            }
-
-            });
+                y: {
+                    beginAtZero: true
+                }}}
+        });
 
     // clinic
     const clinicCtx = document.getElementById('chartClinic');
