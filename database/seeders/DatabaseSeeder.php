8f175dffbbaa56463e83ef3b627432b358b32813
gabriel53 <copperfieldcover@gmail.com>
2022-08-27 21:55:00 +0800

    created an account for admin and superadmin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,21 @@ class DatabaseSeeder extends Seeder
         $this->call(LaratrustSeeder::class);
         $this->call(MedicineSeeder::class);
         $this->call(UserPatientSeeder::class);
+
+        $adminUser = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin.clinic@psu.edu',
+            'password' => Hash::make('admin.clinic@psu.edu'),
+        ]);
+
+         $adminUser->attachRole('administrator');
+
+        $superadminUser = \App\Models\User::factory()->create([
+            'name' => 'Superadmin',
+            'email' => 'superadmin.clinic@psu.edu',
+            'password' => Hash::make('superadmin.clinic@psu.edu'),
+        ]);
+
+         $superadminUser->attachRole('superadministrator');
     }
 }
