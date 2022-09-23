8337e738234bfff7671b081ab6be43813b776f8c
gabriel53 <copperfieldcover@gmail.com>
2022-09-24 00:00:59 +0800

    added logo in mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,21 +1,15 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-slate-200 z-30">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-end h-16">
-            {{-- <div class="flex">
+        <div class="flex justify-between h-16">
+            <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex lg:hidden shrink-0 items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div> --}}
+            </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden lg:block mt-6">
