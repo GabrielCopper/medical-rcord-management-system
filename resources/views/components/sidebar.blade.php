7657f2f4fff60eb5512bfb601cb06caf35aa338b
gabriel53 <copperfieldcover@gmail.com>
2022-10-07 18:07:42 +0800

    changed icon for reports

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -121,8 +121,9 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                         style="fill: rgba(148, 163, 184, 1);transform: ;msFilter:;">
                         <path
-                            d="M6 22h12a2 2 0 0 0 2-2V8l-6-6H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2zm7-18 5 5h-5V4zM8 14h3v-3h2v3h3v2h-3v3h-2v-3H8v-2z">
+                            d="M19.903 8.586a.997.997 0 0 0-.196-.293l-6-6a.997.997 0 0 0-.293-.196c-.03-.014-.062-.022-.094-.033a.991.991 0 0 0-.259-.051C13.04 2.011 13.021 2 13 2H6c-1.103 0-2 .897-2 2v16c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V9c0-.021-.011-.04-.013-.062a.952.952 0 0 0-.051-.259c-.01-.032-.019-.063-.033-.093zM16.586 8H14V5.414L16.586 8zM6 20V4h6v5a1 1 0 0 0 1 1h5l.002 10H6z">
                         </path>
+                        <path d="M8 12h8v2H8zm0 4h8v2H8zm0-8h2v2H8z"></path>
                     </svg>
                     <span class="text-sm font-medium ml-3 lg:opacity-100 2xl:opacity-100 duration-200">
                         {{ __('Reports') }}
