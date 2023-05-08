e1dac0a75f3319a0b78674de1a0adf9412bae791
gabriel53 <copperfieldcover@gmail.com>
2023-05-08 11:30:11 +0800

    feat: export data medical record

--- app/Http/Controllers/ExportMedicalRecordController.php
+++ app/Http/Controllers/ExportMedicalRecordController.php
@@ -77,58 +77,79 @@ class ExportMedicalRecordController extends Controller
         $phpWord->addTableStyle('table', $table);
         $table = $section->addTable('table');
         $table->addRow();
-        $table->addCell(1750)->addText(htmlspecialchars("#"));
         $table->addCell(1750)->addText(htmlspecialchars("Medicines Given or Distributed"));
+        $table->addCell(1750)->addText(htmlspecialchars("No. of Medicines Given"));
         $table->addCell(1750)->addText(htmlspecialchars("No. of Students"));
         $table->addCell(1750)->addText(htmlspecialchars("No. of Faculty"));
         $table->addCell(1750)->addText(htmlspecialchars("No. of Staff"));
-            foreach($datas as $data) {
-        /*     $school_year = $data->school_year->school_year;
-        $consult_date = \Carbon\Carbon::parse($data->patient_consult_date)->isoFormat('MMM D YYYY');
-            $semester = $data->semester; */
 
-            $medicines = Str::of($data->patient_prescribed_medicine)->explode('|');
-            $quantity = Str::of($data->patient_prescribed_medicine_quantity)->explode('|');
-            $student_value = '';
-            if ($data->user_data->user_patient_role === 'student') {
-                $student_value = '1';
-            } else {
-                $student_value = '0';
+
+        // logic
+                $medicineQuantities = [
+            'students' => 0,
+            'faculty' => 0,
+            'staff' => 0,
+            ];
+            $medicines = [];
+            $totalStudents = 0;
+            $totalFaculty = 0;
+            $totalStaff = 0;
+            $totalMedicineQuantity = 0;
+
+            foreach ($datas as $data) {
+            $prescribedMedicines = explode('|', $data->patient_prescribed_medicine);
+            $prescribedQuantities = explode('|', $data->patient_prescribed_medicine_quantity);
+            $role = $data->user_data->user_patient_role;
+
+            foreach ($prescribedMedicines as $index => $medicine) {
+            $quantity = (int)$prescribedQuantities[$index];
+
+            if ($quantity == 0) {
+            continue; // skip adding quantity if it's 0
             }
 
-            $faculty_value = '';
-            if ($data->user_data->user_patient_role === 'teaching_staff') {
-                $faculty_value = '1';
-            } else {
-                $faculty_value = '0';
+            if (!array_key_exists($medicine, $medicines)) {
+            $medicines[$medicine] = [
+            'quantity' => 0,
+            'students' => 0,
+            'faculty' => 0,
+            'staff' => 0,
+            ];
             }
 
-            $staff_value = '';
-            if ($data->user_data->user_patient_role === 'non_teaching_staff') {
-                $staff_value = '1';
-            } else {
-                $staff_value = '0';
-            }
+            $medicines[$medicine]['quantity'] += $quantity;
 
+
+            if ($role === 'student') {
+            $medicines[$medicine]['students'] += 1;
+            $totalStudents += 1;
+            } elseif ($role === 'teaching_staff') {
+            $medicines[$medicine]['faculty'] += 1;
+            $totalFaculty += 1;
+            } elseif ($role === 'non_teaching_staff') {
+                $medicines[$medicine]['staff'] += 1;
+                $totalStaff += 1;
+            }
+        }}
+
+            foreach ($medicines as $medicine) {
+            $totalMedicineQuantity += $medicine['quantity'];
+        }
+
+        foreach($medicines as $medicine => $values) {
             $table->addRow();
-            $table->addCell(2000)->addText($quantity->implode(', '));
-             if($data->patient_prescribed_medicine == '') {
-                 $table->addCell(2000)->addText(htmlspecialchars("No Prescribed Medicine"));
-            } else {
-            $table->addCell(2000)->addText($medicines->implode(', '));
-
-            }
-            $table->addCell(2000)->addText($student_value );
-            $table->addCell(2000)->addText($faculty_value);
-            $table->addCell(2000)->addText($staff_value);
-       /*   */
+            $table->addCell(2000)->addText($medicine);
+            $table->addCell(2000)->addText($values['quantity'] );
+            $table->addCell(2000)->addText($values['students'] );
+            $table->addCell(2000)->addText($values['faculty'] );
+            $table->addCell(2000)->addText($values['staff'] );
         }
         $table->addRow();
-        $table->addCell(1750)->addText(htmlspecialchars("No. of Medicines Given"));
-        $table->addCell(1750)->addText(htmlspecialchars(""));
-        $table->addCell(1750)->addText($student);
-        $table->addCell(1750)->addText($total_teaching_staff);
-        $table->addCell(1750)->addText($staff);
+        $table->addCell(1750)->addText(htmlspecialchars("TOTAL"));
+        $table->addCell(1750)->addText($totalMedicineQuantity);
+        $table->addCell(1750)->addText($totalStudents);
+        $table->addCell(1750)->addText($totalFaculty);
+        $table->addCell(1750)->addText($totalStaff);
 
         \PhpOffice\PhpWord\Settings::setZipClass(Settings::PCLZIP);
 
