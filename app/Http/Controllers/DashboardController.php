d2395272b2657780812888983f953cd3c1ba7b65
gabriel53 <copperfieldcover@gmail.com>
2023-07-23 17:06:00 +0800

    fix: fix dashboard stat count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,9 +14,15 @@ class DashboardController extends Controller
     public function index()
     {
         if (Auth::user()->hasRole('administrator')) {
-            $teaching_staffs_count = UserPatient::where('user_patient_role', 'teaching_staff')->count();
-            $non_teaching_staffs_count = UserPatient::where('user_patient_role', 'non_teaching_staff')->count();
-            $students_count = UserPatient::where('user_patient_role', 'student')->count();
+            $teaching_staffs_count = Patient::with('user_data')->filter(request(['clinic']))->whereHas('user_data', function ($query) {
+                return $query->where('user_patient_role', '=', 'teaching_staff');
+            })->count();
+            $students_count = Patient::with('user_data')->filter(request(['clinic']))->whereHas('user_data', function ($query) {
+                return $query->where('user_patient_role', '=', 'student');
+            })->count();
+            $non_teaching_staffs_count = Patient::with('user_data')->filter(request(['clinic']))->whereHas('user_data', function ($query) {
+                return $query->where('user_patient_role', '=', 'non_teaching_staff');
+            })->count();
             return view('pages.admin.dashboard.index', compact('teaching_staffs_count',
                  'non_teaching_staffs_count', 'students_count'));
         } elseif (Auth::user()->hasRole('superadministrator')) {
